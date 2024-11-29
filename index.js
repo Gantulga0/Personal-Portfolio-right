300d29eb1c247744d1d2e365b88a0fb781f6c488
24LP9558 24LP9558 <tudewdagwagantulga@gmail.com>
2024-11-29 11:12:18 +0800

    dark mode done

---
+++ index.js
@@ -0,0 +1,22 @@
+const toggleButton = document.getElementById('toggle-button')
+const lightModeIcon = document.getElementById('light-mode')
+const darkModeIcon = document.getElementById('dark-mode')
+
+let isLightMode = true
+
+toggleButton.addEventListener('click', () => {
+    if (isLightMode) {
+        darkModeIcon.style.display = 'flex'
+        lightModeIcon.style.display = 'none'
+        isLightMode = false
+
+    } else {
+        darkModeIcon.style.display = 'none'
+        lightModeIcon.style.display = 'flex'
+        isLightMode = true   
+    }
+    const currentTheme= document.documentElement.getAttribute('data-theme');
+    const newTheme= currentTheme === 'light' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme',newTheme);
+})
+
